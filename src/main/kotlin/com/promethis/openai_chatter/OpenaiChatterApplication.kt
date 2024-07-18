90dbc66461384821d45a2b24a2454a08d477e161
Shapur1234 <48966182+Shapur1234@users.noreply.github.com>
2024-07-18 10:24:56 +0200

    Initial web server infrastructure

--- src/main/kotlin/com/promethis/openai_chatter/OpenaiChatterApplication.kt
+++ src/main/kotlin/com/promethis/openai_chatter/OpenaiChatterApplication.kt
@@ -2,13 +2,34 @@ package com.promethis.openai_chatter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+data class Message(val name: String, val text: String)
+
+@RestController
+class MessageController {
+    @GetMapping("/chat")
+    fun index(@RequestParam("name") name: String = "", @RequestParam("text") text: String = "") : String {
+        if (name.isBlank()) {
+            return "`text` was not set"
+        }
+        if (text.isBlank()) {
+            return "`text` was not set"
+        }
+
+        return "${Message(name, name)}"
+    }
+}
 
 @SpringBootApplication
 class OpenaiChatterApplication
 
 fun main(args: Array<String>) {
-    val apiKey = (System.getenv("OPENAI_API_KEY")
-        ?: "").ifEmpty { throw IllegalArgumentException("env var OPENAI_API_KEY is not set") }
+//    val apiKey = (System.getenv("OPENAI_API_KEY")
+//        ?: "").ifEmpty { throw IllegalArgumentException("env var OPENAI_API_KEY is not set") }
+    val apiKey = "demo"
 
     runApplication<OpenaiChatterApplication>(*args)
 }
