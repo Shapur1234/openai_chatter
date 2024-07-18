aeba6621b6503ffba0c2055bb5df4cc145cf427b
Shapur1234 <48966182+Shapur1234@users.noreply.github.com>
2024-07-18 13:46:24 +0200

    Merge Main.kt to Application.Kt

--- src/main/kotlin/ai/promethist/openaichatter/Application.kt
+++ src/main/kotlin/ai/promethist/openaichatter/Application.kt
@@ -1,6 +1,11 @@
 package ai.promethist.openaichatter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
--- src/main/kotlin/ai/promethist/openaichatter/Main.kt
+++
@@ -1,7 +0,0 @@
-package ai.promethist.openaichatter
-
-import org.springframework.boot.runApplication
-
-fun main(args: Array<String>) {
-    runApplication<Application>(*args)
-}
