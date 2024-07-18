e7556115c48bc49aba6315e57e0ce8e86b82ab11
Shapur1234 <48966182+Shapur1234@users.noreply.github.com>
2024-07-18 10:57:14 +0200

    Initial working prototype

--- src/main/kotlin/com/promethis/openai_chatter/OpenaiChatterApplication.kt
+++ src/main/kotlin/com/promethis/openai_chatter/OpenaiChatterApplication.kt
@@ -5,21 +5,40 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import dev.langchain4j.model.chat.ChatLanguageModel
+import dev.langchain4j.model.openai.OpenAiChatModel
+import dev.langchain4j.model.openai.OpenAiChatModelName.GPT_3_5_TURBO
 
-data class Message(val name: String, val text: String)
+data class Question(val name: String, val question: String)
+
+class AIManager {
+    //    private val apiKey = (System.getenv("OPENAI_API_KEY")
+    //        ?: "").ifEmpty { throw IllegalArgumentException("env var OPENAI_API_KEY is not set") }
+    private val apiKey = "demo"
+    private val model: ChatLanguageModel = OpenAiChatModel.builder()
+        .apiKey(apiKey)
+        .modelName(GPT_3_5_TURBO)
+        .maxTokens(50)
+        .build();
+
+    fun get_responce(question: Question): String =
+        model.generate("Greet the user ${question.name} and answer his question: \"${question.question}\"")
+}
 
 @RestController
 class MessageController {
-    @GetMapping("/chat")
-    fun index(@RequestParam("name") name: String = "", @RequestParam("text") text: String = "") : String {
-        if (name.isBlank()) {
-            return "`text` was not set"
-        }
-        if (text.isBlank()) {
-            return "`text` was not set"
-        }
 
-        return "${Message(name, name)}"
+    @GetMapping("/chat")
+    fun index(@RequestParam("name") name: String = "", @RequestParam("question") question: String = ""): String {
+        if (name.isEmpty() || name.isBlank()) {
+            return "`question` was not set"
+        }
+        if (question.isEmpty() || question.isBlank()) {
+            return "`text` was not set"
+        }
+        val aiManager = AIManager()
+
+        return aiManager.get_responce(Question(name, question))
     }
 }
 
