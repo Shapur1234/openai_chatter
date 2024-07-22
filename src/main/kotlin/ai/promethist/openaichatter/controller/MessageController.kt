0e5a5c8ba639400f1702afbecf97d7e384e1ecf4
Shapur1234 <48966182+Shapur1234@users.noreply.github.com>
2024-07-22 14:04:21 +0200

    Send HTTP satus code on bad request

--- src/main/kotlin/ai/promethist/openaichatter/controller/MessageController.kt
+++ src/main/kotlin/ai/promethist/openaichatter/controller/MessageController.kt
@@ -3,25 +3,28 @@ package ai.promethist.openaichatter.controller
 import ai.promethist.openaichatter.service.AIManager
 import ai.promethist.openaichatter.model.Query
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/chat")
 class MessageController(private val aiManager: AIManager) {
-    @GetMapping("/chat")
-    fun index(
+    @GetMapping
+    fun chat(
         @RequestParam("name") name: String = "",
         @RequestParam("text") text: String = "",
         @RequestParam("sessionId") sessionId: Long? = null
-    ): Any {
-        if (name.isEmpty() || name.isBlank()) {
-            return "`name` was not set"
-        }
-        if (text.isEmpty() || text.isBlank()) {
-            return "`text` was not set"
+    ): ResponseEntity<Any> {
+        if (name.isEmpty() || name.isBlank() || text.isEmpty() || text.isBlank()) {
+            return ResponseEntity(HttpStatus.BAD_REQUEST)
         }
 
-        return aiManager.getResponse(Query(name, text, sessionId))
+        return ResponseEntity.ok(
+            aiManager.getResponse(Query(name, text, sessionId))
+        )
     }
 }
